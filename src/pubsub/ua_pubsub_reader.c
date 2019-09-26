5ab68c9f546bd2ec41375430a12788780719e771
Shriya Chaurasia <shriya.c@kalycito.com>
2019-09-26 14:53:54 +0530

    PubSub: Add ReaderGroup and DataSetReader objects in Information model
     - Add removeReaderGroupRepresentation and
       removeDataSetReaderRepresentation APIs
     - Update dataSetWriterId and writerGroupId
     - Call dataSetReader destructor
     - TODO: Add subscribedDataSet under dataSetReader
    
    Change-Id: I3db204e80bdb5f1a0ca8d2c2dfe30616fdf1a86a

--- src/pubsub/ua_pubsub_reader.c
+++ src/pubsub/ua_pubsub_reader.c
@@ -91,7 +91,7 @@ UA_Server_removeReaderGroup(UA_Server *server, UA_NodeId groupIdentifier) {
     /* Unregister subscribe callback */
     UA_PubSubManager_removeRepeatedPubSubCallback(server, readerGroup->subscribeCallbackId);
 #ifdef UA_ENABLE_PUBSUB_INFORMATIONMODEL
-    /* To Do:RemoveGroupRepresentation(server, &readerGroup->identifier) */
+    removeReaderGroupRepresentation(server, readerGroup);
 #endif
 
     /* UA_Server_ReaderGroup_delete also removes itself from the list */
