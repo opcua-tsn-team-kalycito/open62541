74cfcbc267262c81883915dce6466b96a4c55dab
Keerthivasan Alagarsamy Senthilkumaran <keerthivasan.as@kalycito.com>
2019-11-11 12:52:37 +0530

    feat(PubSub): Multithread PubSub TSN application
     - Application design change to run Publisher
       and Subscriber in same core(core 2)
     - User application runs in core 3
     - Three thread application design
       (Publisher, Subscriber, User application)
     - TODO:Implement spin locks for threads
    
    Change-Id: I7c4a9bc685c9395d058b4cce71cb1ea9ffef61a8

--- plugins/ua_pubsub_realtime.c
+++ plugins/ua_pubsub_realtime.c
@@ -25,17 +25,15 @@
 #include <netinet/ether.h>
 
 #define       CLOCKIDENTITY                                     CLOCK_TAI
-#define       CYCLE_TIME                                        250 * 1000
+#define       CYCLE_TIME                                        200 * 1000
 #define       SECONDS                                           1000 * 1000 * 1000
-#define       SECONDS_INCREMENT                                 1
-#define       FAILURE_EXIT                                     -1
+#define       SECONDS_INCREMENT                                  1
+#define       FAILURE_EXIT                                      -1
 #define       SHIFT_32BITS                                      32
 
 #ifndef       SOCKET_TRANSMISSION_TIME
 #define       SOCKET_TRANSMISSION_TIME                          61
-#ifndef       SCM_TXTIME
 #define       SCM_TXTIME                                        SOCKET_TRANSMISSION_TIME
-#endif
 #endif
 
 #ifndef       SOCKET_EE_ORIGIN_TRANSMISSION_TIME
@@ -54,8 +52,7 @@ UA_Boolean             firstPacket     = UA_TRUE;
 ssize_t                dataCount;
 UA_Int32               errorCount;
 __u64                  txtime;
-/* Qbv offset in us. Eg: For 25us: 25*1000 */
-/* To make the packet available at offset of 25us(in window 1) */
+/* Qbv offset is 5us for i5. For Mbox, qbv offset is 25us */
 __u64                  qbv_offset      = 25 * 1000;
 static UA_Int32        txTimeEnable    = 1;
 static unsigned char txBuffer[256];
@@ -144,7 +141,6 @@ static int sockErrorQueueProcess(int fd) {
     unsigned char errorBuffer[sizeof(txBuffer)];
     /* Structure for storing the error data */
     struct cmsghdr* controlErrorMsg;
-    __u64 timeStamp;
 
     /* Structure for gathering of input/output error buffers */
     struct iovec inputOutputErrorVec = {
@@ -171,6 +167,7 @@ static int sockErrorQueueProcess(int fd) {
         struct sock_extended_err* sockErrorQueue;
         sockErrorQueue = (struct sock_extended_err *) CMSG_DATA(controlErrorMsg);
         if (sockErrorQueue->ee_origin == SOCKET_EE_ORIGIN_TRANSMISSION_TIME) {
+            __u64 timeStamp = 0;
             timeStamp = ((__u64) sockErrorQueue->ee_data << SHIFT_32BITS) + sockErrorQueue->ee_info;
             switch (sockErrorQueue->ee_code) {
             case SOCKET_EE_CODE_TRANSMISSION_TIME_INVALID_PARAM:
@@ -211,8 +208,7 @@ txtimecalc_udp(UA_PubSubChannel *channel, UA_ExtensionObject *transportSettigns,
     if (firstPacket == UA_TRUE)
    {
         clock_gettime(CLOCKIDENTITY, &nextCycleStartTime);
-        nextCycleStartTime.tv_sec += START_TIME_BOUNDARY;
-        nextCycleStartTime.tv_nsec = 0;
+        nextCycleStartTime.tv_nsec = CYCLE_TIME + (__syscall_slong_t)qbv_offset;
         firstPacket = UA_FALSE;
     }
     else
@@ -221,19 +217,15 @@ txtimecalc_udp(UA_PubSubChannel *channel, UA_ExtensionObject *transportSettigns,
         nanoSecondFieldConversion(&nextCycleStartTime);
     }
     struct sockaddr_ll sll = {0};
-    /* Calculate the txtime and use txtime to publish the packet at the configured time
-     * with qbv_offset */
+/* Calculate the txtime and use txtime to publish the packet at the configured time */
     txtime = (long long unsigned int)nextCycleStartTime.tv_sec * SECONDS + (long long unsigned int)nextCycleStartTime.tv_nsec;
-    /* Realtime publisher is scheduled to be launched at a offset
-     * of 25us in window 1 (between 0 to 62.5us) */
-    txtime += qbv_offset;
     if (errorCount == 0) {
         dataCount = sendfunc(channel->sockfd, buf->data, (UA_Int32)buf->length, txtime, sll);
         if (dataCount != (UA_Int32)(buf->length)) {
             return UA_STATUSCODE_BADINTERNALERROR;
          }
 
-    /* Check if errors are pending on the error queue. */
+/* Check if errors are pending on the error queue. */
     errorQueueCheck = poll(&p_fd, 1, 0);
     if (errorQueueCheck == 1 && p_fd.revents & POLLERR) {
         if (!sockErrorQueueProcess(channel->sockfd))
@@ -254,8 +246,8 @@ txtimecalc_ethernet(UA_PubSubChannel *channel, UA_ExtensionObject *transportSett
     if (firstPacket == UA_TRUE)
     {
         clock_gettime(CLOCKIDENTITY, &nextCycleStartTime);
-        nextCycleStartTime.tv_sec += START_TIME_BOUNDARY;
-        nextCycleStartTime.tv_nsec = 0;
+
+        nextCycleStartTime.tv_nsec = CYCLE_TIME + (__syscall_slong_t)qbv_offset;
         firstPacket = UA_FALSE;
     }
     else
@@ -264,16 +256,13 @@ txtimecalc_ethernet(UA_PubSubChannel *channel, UA_ExtensionObject *transportSett
         nanoSecondFieldConversion(&nextCycleStartTime);
    }
 
-    /* Calculate the txtime and use txtime to publish the packet at the configured time
-     * with qbv_offset */
+/* Calculate the txtime and use txtime to publish the packet at the configured time */
     txtime = (long long unsigned int)nextCycleStartTime.tv_sec * SECONDS + (long long unsigned int)nextCycleStartTime.tv_nsec;
-    /* Realtime publisher is scheduled to be launched at a offset
-     * of 25us in window 1 (between 0 to 62.5us) */
-    txtime += qbv_offset;
+
     if (errorCount == 0) {
         dataCount = sendfunc(channel->sockfd, bufSend, (int)lenBuf, txtime, sll);
 
-    /* Check if errors are pending on the error queue. */
+/* Check if errors are pending on the error queue. */
     errorQueueCheck = poll(&p_fd, 1, 0);
     if (errorQueueCheck == 1 && p_fd.revents & POLLERR) {
         if (!sockErrorQueueProcess(channel->sockfd))
